93118e57372b0f6ec941fad5c19c7a0d4e203f8f
Danish The Coder <danishnew80@gmail.com>
2025-04-27 15:06:36 +0500

    Initial file

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* Write a program to print Hello Students on the screen.
+ Write a program to print Hello in the first line and Students in the second line.
+ Write a program to print “MySirG” on the screen. (Remember to print in double quotes)*/
+
+/*#include<stdio.h>
+int main()
+{
+    printf("Hello\n");
+    printf("Student\n");
+    printf("\"MY Sir g\"");
+    return 0;
+}*/
---
+++ practice.c
@@ -0,0 +1,103 @@
+                                        // Question#3
+
+/*WAP to find the area of the circle. Take radius of circle from user as input and print the
+result in below given format.
+Expected output format – “Area of circle is A having the radius R”. Replace A with are & R with radius*/
+
+
+/*#include<stdio.h>
+int main()
+{
+    int r;
+    float Area;
+    printf("Enter the radious");
+    scanf("%d" ,&r);
+    Area=3.14*r*r;
+    printf("Area of circle is %f and radious %d",Area , r);
+    return 0;
+}*/
+
+
+
+
+
+                                            //Question#4
+
+
+/*#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[]="samja karo danish bhai ab bhi na mehnat karo s g ko hasil karn aka liya ";
+    printf("%lu", strlen(s));
+    return 0;
+
+}*/
+
+
+                                        //Question#5
+
+ /*WAP to print the name of the user in double quotes.
+ Expected output format – “Hello , Amit Kumar”*/
+
+ /*#include<stdio.h>
+ int main()
+ {
+    printf("\"Hello , Amit Kumar\"");
+    return 0;
+ }*/
+
+
+
+
+                                        //Question#5
+// WAP to print “%d” on the screen
+
+/*#include<stdio.h>
+int main()
+{
+    
+    return 0;
+}*/
+
+                                        //Question#7
+ /*#include<stdio.h>
+ int main()
+ {
+    int date,mon,year;
+    printf("Date format is \"DD/MM/YYY\"");
+    printf("Enter Date :");
+    scanf("%d",&date);
+    printf("Enter Month :");
+    scanf("%d",&mon);
+    printf("Enter year :");
+    scanf("%d" ,&year);
+    printf("day is %d and Month is %d and the year is %d" ,date , mon ,year);
+    return 0;
+ }*/
+
+
+                                        //Question#8
+
+/*#include<stdio.h>
+int main()
+{
+    float time;
+    int h,m;
+    printf("Enter time in given format \"HH:MM\"");
+    scanf("%d,%d",&h,&m);
+    printf("The time is %d hour and %d min" ,h,m);
+    return 0;
+
+}*/
+
+
+                                            //Question#9
+#include<stdio.h>
+int main()
+{
+    int x=printf("Day 1 but 1 day people call me Danish the Coder ");
+    printf("%d" ,x);
+    return 0;
+}
+
